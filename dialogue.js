d10216d1a7b877b61e6b9a40cff1fca645cb796d
Thomas Szymczak <you@example.com>
2018-03-18 11:56:59 -0400

    Anwser questions based on a list of questions and potential ways of wording them.

--- dialogue.js
+++ dialogue.js
@@ -12,13 +12,39 @@ async function userSpeak() {
 }
 
 function askQuestion(question) {
+    var questions = [
+        {
+            answer: "The cafeteria is currently open for lunch.",
+            phrasings: [
+                "is the cafeteria open?",
+                "is the cafeteria open right now?",
+            ]
+        },
+        {
+            answer: "The cafeteria is open for breakfast from 7:30 to 9:00, lunch from 11:00 to 2:00, and dinner from 5:00 to 7:00.",
+            phrasings: [
+                "when is the cafeteria open?",
+                "what time is the cafeteria open?",
+                "what are the cafeteria's hours?",
+            ]
+        }
+    ]
+
     lowerQuestion = question.toLowerCase();
-    if (lowerQuestion == "is the cafeteria open?") {
-        alexaSpeak("Yes. The cafteria is currently open for lunch.");
-    } else if (lowerQuestion == "when is the cafeteria open?") {
-        alexaSpeak("The cafeteria is open for breakfast from 7:30 to 9:00, lunch from 11:00 to 2:00, and dinner from 5:00 to 7:00.");
-    } else {
-        alexaSpeak("Sorry, I don't recognize that.");
+
+    var matched = 0;
+    for ( var i = 0; i < questions.length && matched == 0; i++ ) {
+        for ( var j = 0; j < questions[i].phrasings.length && matched == 0; j++ ) {
+            potentialQuestion = questions[i].phrasings[j];
+            if ( lowerQuestion == potentialQuestion ) {
+                alexaSpeak(questions[i].answer);
+                matched = 1;
+            }
+        }
+    }
+
+    if (matched == 0) {
+        alexaSpeak("Sorry, I don't understand that.");
     }
 }
 
